c3424ce6f01da0b2f0692cf139417c0ee7f60b9d
Dami Akindele <114703098+Dahmii@users.noreply.github.com>
2025-02-24 19:29:04 +0100

    hero

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "../../../public/img/adventist-en-centered--black.png";
+import logo from "../../../public/img/adventist-symbol--black.svg";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
