b3c435abe8264551bb9b8dfae6b6c4ff11c076f3
Dami Akindele <damiakin95@gmail.com>
2025-02-24 10:44:05 +0100

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="hero-text">
+        <p>
+          In partnership with <span>His Spirit</span>
+        </p>
+        <h1>
+          A lot can happen if you <span>surrender</span> your life to Jesus
+        </h1>
+        <p>
+          Nothing can be compared to the love of Jesus. For God so loved the
+          world that he gace his one and only son, that whoever believes in him
+          shall not perish but hvae eternal life.
+        </p>
+      </div>
+
+      <div className="hero-btn">Accept Jesus</div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../../public/img/adventist-en-centered--black.png";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+
+      <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      <ul className={menuOpen ? "active" : ""}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
+        <li>
+          <a href="#">Our Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
