039d0c77f15c065f9dcfc409630e58977ec08188
Dami Akindele <114703098+Dahmii@users.noreply.github.com>
2025-02-25 17:54:52 +0100

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Join from "./Components/Join/Join";
+import Banner from "./Components/Banner/Banner";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Join />
+      <Banner />
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Banner.css";
+import BlurText from "./BlurText"; // Import the BlurText component
+
+const Banner = () => {
+  return (
+    <section className="banner-container">
+      <div className="banner-text">
+        <h1>
+          <BlurText
+            text="Saturdays"
+            animateBy="words"
+            direction="top"
+            delay={200}
+          />
+        </h1>
+        <p>at 8:30 AM</p>
+        <p>
+          <a href="#">Online</a> or in person
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
