901aa4374cef9af17a42c560429d4da22d958950
Dami Akindele <114703098+Dahmii@users.noreply.github.com>
2025-02-26 14:19:06 +0100

    added fade

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import "./Hero.css";
+import { Fade } from "react-awesome-reveal";
 
 const Hero = () => {
   return (
     <section className="hero-container">
-      <div className="hero-text">
-        <p>
-          In partnership with <span>His Spirit</span>
-        </p>
-        <h1>
-          A lot can happen if you <span>surrender</span> your life to Jesus
-        </h1>
-        <p>
-          Nothing can be compared to the love of Jesus. For God so loved the
-          world that he gace his one and only son, that whoever believes in him
-          shall not perish but have eternal life.
-        </p>
-      </div>
+      <Fade duration={3000}>
+        <div className="hero-text">
+          <p>
+            In partnership with <span>His Spirit</span>
+          </p>
+          <h1>
+            A lot can happen if you <span>surrender</span> your life to Jesus
+          </h1>
+          <p>
+            Nothing can be compared to the love of Jesus. For God so loved the
+            world that he gave his one and only son, that whoever believes in
+            him shall not perish but have eternal life.
+          </p>
+        </div>
 
-      <div className="hero-btn">Accept Jesus</div>
+        <div className="hero-btn">Accept Jesus</div>
+      </Fade>
     </section>
   );
 };
--- src/Components/Join/Join.jsx
+++ src/Components/Join/Join.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Join.css";
 import joinUsImage from "../../../public/img/join-us.png";
+import { Fade } from "react-awesome-reveal";
 
 const Join = () => {
   return (
     <section className="join-container">
-      <div className="join-content">
-        <img src={joinUsImage} alt="Join Us" />
+      <Fade duration={3000}>
+        <div className="join-content">
+          <img src={joinUsImage} alt="Join Us" />
 
-        <div className="join-text">
-          <h1>
-            Join us in <span>worship</span> as we lift our hearts to God
-          </h1>
-          <div className="join-btn">worship with us</div>
+          <div className="join-text">
+            <h1>
+              Join us in <span>worship</span> as we lift our hearts to God
+            </h1>
+            <div className="join-btn">worship with us</div>
+          </div>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 };
